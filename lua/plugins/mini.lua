3abf546801b127e94fc61c06005060a642d64fd0
bdreaves04 <bdreaves04@gmail.com>
2025-01-10 18:44:14 +0000

    using mini.ai

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,8 +3,8 @@ return {
     'echasnovski/mini.nvim',
     version = false,
     config = function()
-      local pairs = require 'mini.pairs'
-      pairs.setup {}
+      require('mini.pairs').setup()
+      require('mini.ai').setup()
     end
   }
 }

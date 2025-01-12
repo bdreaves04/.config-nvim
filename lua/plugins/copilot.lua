60bbb2b0d7cc52b741168c72ca15a9e902473960
bdreaves04 <bdreaves04@gmail.com>
2025-01-12 10:55:27 +0000

    copilot now automatically suggests

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,7 +3,11 @@ return {
     "zbirenbaum/copilot.lua",
     event = "InsertEnter",
     config = function()
-      require('copilot').setup()
+      require('copilot').setup({
+        suggestion = {
+          auto_trigger = true,
+        }
+      })
     end
   }
 }
